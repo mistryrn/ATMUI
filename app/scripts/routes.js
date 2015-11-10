e9364953d728738337cbe2e5c8161f60f7bfa35e
Balraj Shah <BalrajShah@live.com>
2015-11-10 17:45:42 -0500

    Problem with views not showing fixed

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -5,11 +5,11 @@ angular.module('myApp', ['ngRoute'])
 config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl:'../views/view1.html',
+      templateUrl:'/app/views/view1.html',
       controller: 'View1Ctrl'
     })
     .when('/view2', {
-      templateUrl: '../views/view2.html',
+      templateUrl: '/app/views/view2.html',
       controller: 'View2Ctrl'
     })
     .otherwise({

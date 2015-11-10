9449f53031520f60a802a76fbb103f0eb91c18a4
Rakesh Mistry <mistryrn@mcmaster.ca>
2015-11-10 18:40:59 -0500

    Added Virtual Keyboard files in case we need them.
    + VirtualKeyboard/index.html - contains a basic layout for buttons
    + VirtualKeyboard/style.css - contains basic styling for a bland vkb
    + VirtualKeyboard/functionality.js - pure JS to log which button is clicked

---
+++ VirtualKeyboard/functionality.js
@@ -0,0 +1,50 @@
+window.onload = function() {
+    var btn1 = document.getElementById('vkb-btn-1');
+    var btn2 = document.getElementById('vkb-btn-2');
+    var btn3 = document.getElementById('vkb-btn-3');
+    var btn4 = document.getElementById('vkb-btn-4');
+    var btn5 = document.getElementById('vkb-btn-5');
+    var btn6 = document.getElementById('vkb-btn-6');
+    var btn7 = document.getElementById('vkb-btn-7');
+    var btn8 = document.getElementById('vkb-btn-8');
+    var btn9 = document.getElementById('vkb-btn-9');
+    var btnD = document.getElementById('vkb-btn-del');
+    var btn0 = document.getElementById('vkb-btn-0');
+    var btnE = document.getElementById('vkb-btn-enter');
+    btn1.onclick = function (e) {
+        console.log("1");
+    };
+    btn2.onclick = function (e) {
+        console.log("2");
+    };
+    btn3.onclick = function (e) {
+        console.log("3");
+    };
+    btn4.onclick = function (e) {
+        console.log("4");
+    };
+    btn5.onclick = function (e) {
+        console.log("5");
+    };
+    btn6.onclick = function (e) {
+        console.log("6");
+    };
+    btn7.onclick = function (e) {
+        console.log("7");
+    };
+    btn8.onclick = function (e) {
+        console.log("8");
+    };
+    btn9.onclick = function (e) {
+        console.log("9");
+    };
+    btnD.onclick = function (e) {
+        console.log("DEL");
+    };
+    btn0.onclick = function (e) {
+        console.log("0");
+    };
+    btnE.onclick = function (e) {
+        console.log("Enter");
+    };
+};
